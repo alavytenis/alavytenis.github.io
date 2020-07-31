b100fa2f42aa2e91447e405718b07b5abd902890
vytenis <vytenis.alaburda@mif.stud.vu.lt>
2020-07-31 11:58:23 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-761a8abe570525f54d02.js"
+    "url": "webpack-runtime-7c8b37220541786fbb40.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "440bc296d558c3e824708d106a274fe6"
+    "revision": "2344d099ea9427f54cacf7aa8a585f39"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
