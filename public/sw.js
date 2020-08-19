6a0bcbff776e61930864a85e17ed6618998de0ad
vytenis <vytenis.alaburda@mif.stud.vu.lt>
2020-08-19 14:15:09 +0300

    update

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-761a8abe570525f54d02.js"
+    "url": "webpack-runtime-c11b3004fc1e7e6ea6f1.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "440bc296d558c3e824708d106a274fe6"
+    "revision": "edd362d9c75d4832e5f0f26813f1903e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Layout from '../components/Layout';
 import PageFooter from '../components/PageFooter';
@@ -20,7 +21,7 @@ import fishcoin from '../assets/images/projects/fishcoin.png'
 import lietuvosbankas from '../assets/images/projects/lietuvos-bankas.png'
 import pickvibe from '../assets/images/projects/pickvibe.png'
 import fixNetwork from '../assets/images/projects/fix-network.png'
-
+import ShowMore from 'react-show-more';
 
 const sections = [
   { id: 'top', name: 'INTRO'},
@@ -52,15 +53,16 @@ const IndexPage = () => (
             <h2>| WHO WE ARE</h2>
           </header>
 
-          <p>SUPER HOW is a visionary research and development (R&D) company committed to harnessing the power of blockchain for the advancement of society with next-generation solutions.  SUPER HOW has grown to become Lithuania’s leading private research and development lab focusing on DARQ (Blockchain and Distributed ledger technologies, Artificial Intelligence, extended reality and quantum computing) technologies. These future technologies are the next big technology catalysts for change, and SUPER HOW mission is to be at the horizon of innovations and the R&D engine that transforms future technologies into practical applications for business and everyday life. </p>
+          <p>SUPER HOW is a visionary research and development (R&D) company committed to harnessing the power of blockchain for the advancement of society with next-generation solutions.  SUPER HOW has grown to become Lithuania’s leading private research and development lab focusing on DARQ (Blockchain and Distributed ledger technologies, Artificial Intelligence, extended reality and quantum computing) technologies. These future technologies are the next big technology catalysts for change, and SUPER HOW mission is to be at the horizon of innovations and the R&D engine that transforms future technologies into practical
+             applications for business and everyday life. </p>
           <p>
           Moreover, as a revolutionary technology, blockchain provides a practical solution that allows data and transactions to be recorded in a reliable and verifiable way via the mechanism of the secure and decentralised ledger. The team of SUPER HOW strongly believe that blockchain in convergence with other technologies such as Artificial intelligence is the backbone of almost all the prominent infrastructure with vast potentials in many domains. It can be envisaged that in the near future blockchain technology will be widely used in domains such as healthcare, government, finance, education, manufacturing, transportation, and logistics. Therefore, SUPER HOW aims to deliver strong DARQ research outcomes and help the industry to grow. Our research covers a wide range of areas such as applied sciences, aimed at the development and evaluation of new generic blockchain concepts, and application-driven, motivated by important application areas, such as transaction processing, e-health, digital identity protection, supply chain and more. Much of it is experimental, validating the proposed new concepts by means of implementation and deployment in prototypes that are used in the real world.
           </p>
           <p>
           SUPER HOW collaborates with the major stakeholders (local universities, institutes and research labs) in the blockchain and artificial intelligence ecosystem and provides key R&D capabilities in the field. SUPER HOW offers internships for students and researchers the opportunity to gain practical knowledge, by participating in the end-to-end lifecycle of Analysis, Research, Design and Implementation of innovative solutions.
+
           </p>
           <div className="row">
-
           </div>
         </div>
       </section>
@@ -158,23 +160,85 @@ const IndexPage = () => (
           </header>
           <p> We are hiring! </p>
           <div className="career-container">
+            <a href="#popup1">
             <div className="career-card">
               BLOCKCHAIN DEVELOPER
-              <a href="#" className="button career-button">See more</a>
             </div>
-            <div className="career-card">
-              C++ DEVELOPER
+            </a>
+            <a href="#popup1"><div className="career-card">
+              BLOCKCHAIN DEVELOPER
             </div>
-            <div className="career-card">
-              FULL STACK DEVELOPER
+            </a>
+            <a href="#popup1"><div className="career-card">
+              BLOCKCHAIN DEVELOPER
             </div>
-            <div className="career-card">
-              UI/UX DESIGNER
+            </a>
+            <a href="#popup1"><div className="career-card">
+              BLOCKCHAIN DEVELOPER
             </div>
+            </a>
           </div>
         </div>
         <div className="bottom-lightning">
           <img src={sixLightningHorizontal} />
+        </div>
+
+        <div id="popup1" class="overlay">
+        	<div class="popup">
+        		<h2 className="p-2">BLOCKCHAIN DEVELOPER</h2>
+        		<a class="close" href="#career">&times;</a>
+            <div class="d-flex flex-row">
+              <div class="first-column d-flex flex-column col-2 p-2">Open position description</div>
+              <div class="d-flex flex-column col-10 p-2">Together with the team of developers, designers, QA and a Product Owner you will work on creating and maintaining blockchain related infrastructure. Your primary responsibility will be analysing requirements, designing blockchain technology according to some business model, and building and launching of a NEM Symbol, Stellar or Hyperledger technology networks (public, consortium and private).</div>
+            </div>
+            <div class="d-flex flex-row">
+              <div class="first-column d-flex flex-column col-2 p-2">Tasks and Responsibilities</div>
+              <div class="d-flex flex-column col-10 p-2">
+                <li>Work with limited or incomplete Hyperledger systems descriptions and manuals;</li>
+                <li>Research, design, develop and test DLT, blockchain and related additional tools;</li>
+                <li>Prepare and maintain server/cloud infrastructure for blockchain needs;</li>
+                <li>Select the best tools and practices to ensure excellent quality;</li>
+                <li>Constant learning and being on the edge of DLT and blockchain technology, application patterns, best practices of architecture design and code implementation;</li>
+                <li>Document new solutions as well as maintain documentation of existing ones.</li>
+              </div>
+            </div>
+            <div class="d-flex flex-row">
+              <div class="first-column d-flex flex-column col-2 p-2">What we expect</div>
+              <div class="d-flex flex-column col-10 p-2">
+                <li>3+ years of experience in software development;</li>
+                <li>Strong knowledge of common and concurrency algorithms and data structures;</li>
+                <li>Familiarity with basic cryptography;</li>
+                <li>Some understanding of Unix systems and server-side scripting;</li>
+                <li>Strong knowledge of at least one scripting language (PHP/Python/JavaScript/you name it);</li>
+                <li>At least some knowledge in object-oriented programming and relational databases;</li>
+                <li>At least some knowledge in NEM Symbol, Stellar, Hyperledger or similar;</li>
+                <li>Good English language skills;</li>
+                <li>Good teamwork skills and familiarity with Git;</li>
+                <li>Confident knowledge of source code analysis and optimisation;</li>
+                <li>Interest in learning and growing know-how in best programming, blockchain and system development practices;</li>
+              </div>
+            </div>
+            <div class="d-flex flex-row">
+              <div class="first-column d-flex flex-column col-2 p-2">Nice to have skills</div>
+              <div class="d-flex flex-column col-10 p-2">
+                <li>Ansible script writing; </li>
+                <li>Experience with C or C++; </li>
+                <li>Knowledge of CI/CD or DevOps;  </li>
+                <li>Previous experience with Go or Java; </li>
+                <li>Experience with Kubernetes or Docker; </li>
+                <li>Experience with large data sets or financial/banking systems;</li>
+              </div>
+            </div>
+            <div class="d-flex flex-row">
+              <div class="first-column d-flex flex-column col-2 p-2">What we offer</div>
+              <div class="d-flex flex-column col-10 p-2">
+                <li>Ideal conditions for work and flexible work hours; </li>
+                <li>Personal incentive system (learning courses of your choice, e-books, stellar idea bonuses, etc.); </li>
+                <li>Great international team; </li>
+                <li>Unique opportunities to grow professionally and as part of the team which is on a path of disrupting the world and changing the future;</li>
+              </div>
+            </div>
+        	</div>
         </div>
       </section>
 
